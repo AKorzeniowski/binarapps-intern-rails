5ddfd40f7148626a2e27944bd9e4c40d20e47715
Schocker <adikorzeniowski@gmail.com>
2018-07-11 14:55:04 +0200

    Add editing and destroying posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit 
+    @post = Post.find(params[:id])
+  end
+
+  def update 
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: 'Post was updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy
+      redirect_to posts_path, notice: 'Post was destroyed'
+    else
+      redirect_to posts_path, alert: 'Post could not be destroyed'
+    end
+  end
+
   private 
   def post_params
     params.require(:post).permit(:title, :content, :author_id)
