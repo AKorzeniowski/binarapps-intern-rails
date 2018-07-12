bf05d3211d51a69f01e8cd0df887b3d0a70d0cc0
Schocker <adikorzeniowski@gmail.com>
2018-07-12 12:34:22 +0200

    Add simple tests for posts

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
   validates :name, :surname, presence: true
-  # validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
 
   # has_many :author_posts
   # has_many :posts, through: :author_posts
@@ -9,7 +9,7 @@ class Author < ActiveRecord::Base
 
   scope :old, -> { where('age > 30') }
 
-  before_create :default_age
+  before_validation :default_age
 
   def fullname
     "#{name} #{surname}"
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before(:all) do
+    @author = Author.create(name: 'test', surname: 'test')
+  end
+
+  it 'should have proper attributes' do
+    expect(subject.attributes).to include('title', 'content', 'author_id')
+  end
+
+  it 'should require title, content and author presence' do
+    expect(Post.new).not_to be_valid
+    expect(Post.new(title: 'Testowy tytuł')).not_to be_valid
+    expect(Post.new(title: 'Testowy tytuł', content: 'Testowy content')).not_to be_valid
+    expect(Post.new(title: 'Testowy tytuł', content: 'Testowy content', author_id: @author.id)).to be_valid
+  end
+
+  it 'should validate length of content' do
+    long_content = ''
+    512.times { long_content << 'x' }
+
+    expect(Post.new(title: 'Testowy tytuł', content: 'Too short')).not_to be_valid
+    expect(Post.new(title: 'Testowy tytuł', content: long_content)).not_to be_valid
+
+    expect(Post.new(title: 'Testowy tytuł', content: 'Prawidłowy content')).to be_valid
+  end
+
+  it 'should have one author' do
+    t = Post.reflect_on_association(:author)
+    expect(t.macro).to eq(:belongs_to)
+  end
+
+  it 'should have many comments' do
+    t = Post.reflect_on_association(:comments)
+    expect(t.macro).to eq(:has_many)
+  end
+end
