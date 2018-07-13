1baa06f688808114d0ea3049512c07224337a095
Schocker <adikorzeniowski@gmail.com>
2018-07-13 15:33:25 +0200

    Add timeout to error so the loading indicator can be seen

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -1,12 +1,10 @@
 $(document).ready( (function() {
-	$('h1').css('color', 'red');
-
 	$('.new_author')
 	.on('ajax:send', function() {
-		$('.new_author').append('<img class="loader" src="https://loading.io/spinners/rolling/lg.curve-bars-loading-indicator.gif">');
+		$('.loader').show();
 	})
 	.on('ajax:complete', function () {
-		$('.loader').remove();
+		$('.loader').hide()
 	})
 
 }));
